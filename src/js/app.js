83c477419f95a9ae2645b5149971dc02b23d8961
Wilber Ballesteros <ballestas01@gmail.com>
2023-09-27 15:03:50 -0500

    implementacion de dark-mode al sitio web

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     eventListeners();
+
+    darkMode();
 });
 
 function eventListeners() {
@@ -19,4 +21,30 @@ function navegacionResponsive() {
 
     //el if se puede resumir en esto, toogle quita y pone una clase
    // navegacion.classList.toggle('mostrar')
+}
+
+function darkMode() {
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+    //console.log(prefiereDarkMode.matches);
+
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton');
+
+    botonDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode'); //quita la clase si la tiene y vicerversa
+    });
 }
